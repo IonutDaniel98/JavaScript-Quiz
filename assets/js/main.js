5e199a811cd8b8da8666b1ff321f18e1109425e6
Ionut Bejenariu <ibejenariu98@yahoo.com>
2020-12-13 20:46:00 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+let questions;
+let questionsCount;
+let currentQuestion;
+
+let question_title_elem = document.getElementById("title");
+let answers_elem = document.getElementById("answers");
+let action_btn = document.getElementById("action_btn");
+
+function getQuestions(){
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            questions = JSON.parse(this.responseText).questions;
+            questionsCount = questions.length;
+            currentQuestion = 0;
+        }
+    }
+    request.open("GET", "/questions.json", false);
+    request.send();
+}
+
+function displayQuestion(question){
+    answers_elem.innerHTML = "";
+    question.answers.forEach(answer => {
+        let label = document.createElement("label");
+
+        let answer_input = document.createElement("input");
+        answer_input.setAttribute("type", "radio");
+        answer_input.setAttribute("name", "answer");
+        answer_input.setAttribute("value", answer.id);
+        answer_input.classList.add("answer");
+
+        let answer_title = document.createTextNode(answer.answer);
+        label.appendChild(answer_input);
+        label.appendChild(answer_title);
+
+        answers_elem.appendChild(label);
+    })
+}
+
+
+//Initialization
+getQuestions();
+displayQuestion(questions[currentQuestion]);
